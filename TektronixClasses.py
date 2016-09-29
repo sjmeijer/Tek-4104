4545fd19ddbd188c71d0d81fe8e1ac6539a00c50
Sam <samuelmeijer@gmail.com>
2016-09-28 20:28:42 -0400

    Generic cleanup, still doesn't end gracefully

--- TektronixClasses.py
+++ TektronixClasses.py
@@ -11,34 +11,47 @@ class Tektronix4104():
     trigger = 'TRIG\n'
 
     def __init__(self,ID):
+        print "Setting up resource manager..."
+        # import pdb; pdb.set_trace()
+
         self.rm = visa.ResourceManager()
+        print "Opening resource..."
         self.scope = self.rm.open_resource(ID)
 
-    def getOffsets(self):
-        self.scope.write('DATA:SOU CH1')
-        self.scope.write('DATA:WIDTH 1')
-        self.scope.write('DATA:ENC RPB')
+    def write(self,arg):
+        return self.scope.write(arg)
 
-        self.ymult = float(self.scope.ask('WFMPRE:YMULT?'))
-        self.yzero = float(self.scope.ask('WFMPRE:YZERO?'))
-        self.yoff  = float(self.scope.ask('WFMPRE:YOFF?'))
-        self.xincr = float(self.scope.ask('WFMPRE:XINCR?'))
+    def query(self,arg):
+        return self.scope.ask(arg)
+
+    def read_raw(self):
+        return self.scope.read_raw()
+
+    def getOffsets(self):
+        self.write('DATA:SOU CH1')
+        self.write('DATA:WIDTH 1')
+        self.write('DATA:ENC RPB')
+
+        self.ymult = float(self.query('WFMPRE:YMULT?'))
+        self.yzero = float(self.query('WFMPRE:YZERO?'))
+        self.yoff  = float(self.query('WFMPRE:YOFF?'))
+        self.xincr = float(self.query('WFMPRE:XINCR?'))
 
         offsets = [self.ymult,self.yzero,self.yoff,self.xincr]
         return offsets
 
     def checkTrigger(self):
-        self.state = self.scope.ask('TRIGGER:STATE?')
+        self.state = self.query('TRIGGER:STATE?')
         return self.state
 
     def setTriggerNorm(self):
-        self.scope.write('TRIGGER:A:MODe NORMAL')
+        self.write('TRIGGER:A:MODe NORMAL')
         return 0
 
     def readData(self):
-        self.scope.write('CURVE?')
-        self.data = self.scope.read_raw()
-        return self.data
+        self.write('CURVE?')
+        self.data = self.read_raw()
+        return np.shape(self.data)
 
     def unpackData(self):
         headerlen = 2 + int(self.data[1])
@@ -46,6 +59,7 @@ class Tektronix4104():
         ADC_wave = self.data[headerlen:-1]
 
         ADC_wave = np.array(unpack('%sB' % len(ADC_wave),ADC_wave))
+
         self.volts = (ADC_wave - self.yoff)*self.ymult + self.yzero
         self.time = np.arange(0,self.xincr*len(self.volts),self.xincr)
 
--- readout.py
+++ readout.py
@@ -1,3 +1,4 @@
+# import time as Time
 import pylab
 import matplotlib.pyplot as plt
 from TektronixClasses import Tektronix4104
@@ -6,7 +7,7 @@ def main():
     try:
         ID = 'TCPIP::152.19.204.238::INSTR'
         oscope = Tektronix4104(ID)
-
+        print "Getting offsets..."
         oscope.getOffsets()
         armed   = oscope.armed
         auto    = oscope.auto
@@ -14,6 +15,7 @@ def main():
         save    = oscope.save
         trigger = oscope.trigger
 
+        print "Checking trigger status..."
         state = oscope.checkTrigger()
 
         plt.ion()
@@ -21,11 +23,13 @@ def main():
 
         triggerCount = 0;
         checkCount = 1;
+        # start = Time.time()
+        # while((Time.time()-start) > 30):
         while(True):
             if(oscope.state == oscope.trigger):
                 triggerCount+=1;
                 print "Scope has triggered %d/%d, reading out data now" % (triggerCount,checkCount)
-                data = oscope.readData()
+                print oscope.readData()
                 [time,volts] = oscope.unpackData()
                 plt.plot(time,volts)
                 plt.show()
@@ -35,10 +39,14 @@ def main():
 
             state = oscope.checkTrigger()
             checkCount+=1
-        rm.close()
+
+    except KeyboardInterrupt:
+        print "Done taking data."
 
     finally:
-        print "closing..."
+        print "Closing..."
+        oscope.rm.close()
+
     return 0
 
 if __name__ == "__main__":
